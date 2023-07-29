5747e7d5c1fc1066b4c8be3ebcc1216d278c1ab3
HeinrichWinkel <139074080+HeinrichWinkel@users.noreply.github.com>
2023-07-29 13:27:24 +0200

    Add files via upload

---
+++ OCRpckgInstaller.ps1
@@ -0,0 +1,13 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+$OCRPackages = Get-WindowsCapability -Online | Where-Object { $_.Name -Like 'Language.OCR*' }
+
+$CountryCodes = $OCRPackages.Name | ForEach-Object { $_ -replace 'Language.OCR~~~', '' } | ForEach-Object { $_ -replace '~.*', '' }
+
+foreach ($CountryCode in $CountryCodes) {
+    $Capability = Get-WindowsCapability -Online | Where-Object { $_.Name -Like "Language.OCR*$CountryCode*" }
+    $Capability | Add-WindowsCapability -Online
+}
